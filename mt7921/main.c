5d8baa4667fd5c7052a6db151aaf4d06f9c39aab
Chen Minqiang <ptpt52@gmail.com>
2021-03-17 01:12:59 +0800

    hack: Disable GTK offloading

--- mt7921/main.c
+++ mt7921/main.c
@@ -414,14 +414,8 @@ static int mt7921_set_key(struct ieee80211_hw *hw, enum set_key_cmd cmd,
 	struct mt76_wcid *wcid = &msta->wcid;
 	int idx = key->keyidx;
 
-	/* The hardware does not support per-STA RX GTK, fallback
-	 * to software mode for these.
-	 */
-	if ((vif->type == NL80211_IFTYPE_ADHOC ||
-	     vif->type == NL80211_IFTYPE_MESH_POINT) &&
-	    (key->cipher == WLAN_CIPHER_SUITE_TKIP ||
-	     key->cipher == WLAN_CIPHER_SUITE_CCMP) &&
-	    !(key->flags & IEEE80211_KEY_FLAG_PAIRWISE))
+	/* Disable GTK offloading */
+	if (!(key->flags & IEEE80211_KEY_FLAG_PAIRWISE))
 		return -EOPNOTSUPP;
 
 	/* fall back to sw encryption for unsupported ciphers */
